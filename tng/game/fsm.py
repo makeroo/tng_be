dfc021099a121143f80fd3502a608d5280f5cc4b
Simone Pierazzini <s.pierazzini@geniodiligence.it>
2023-11-21 14:21:09 +0100

    validate discover tiles move also skip rotation if the tile has no orientation

--- tng/game/fsm.py
+++ tng/game/fsm.py
@@ -216,15 +216,36 @@ class TNGFSM:
         return new_game.set_turn((game.turn + 1) % len(game.players))
 
     def discover_tiles_place_tile(self, game: Game, player: PlayerColor, move: PlaceTile) -> Game:
-        # this is the start macro phase, there is no need
-        # to check for bounds: there are surely tiles to draw
+        # validate
+
+        player_status = game.players[game.turn]
+
+        if player_status.color != player:
+            raise IllegalMove('not player turn')
+
+        if game.draw_index >= len(game.tile_holder):
+            raise IllegalMove('final flickres')
+
+        # apply
+
         placed_tile = game.tile_holder[game.draw_index]
 
-        return (
-            self._apply_place_tile(game, player, move, placed_tile)
-            .new_phase(Phase.rotate_discovered_tile)
-            .draw_tile()
-        )
+        if placed_tile in (Tile.t_passage, Tile.straight_passage, Tile.t_passage):
+            return (
+                self._apply_place_tile(game, player, move, placed_tile)
+                .new_phase(Phase.rotate_discovered_tile)
+                .draw_tile()
+            )
+
+        if player_status.pos is None:
+            raise GameRuntimeError('player without pos')
+
+        cells = game.board.visible_cells_from(player_status.pos)
+
+        if any(cell.tile is None for cell in cells):
+            return game.new_phase(Phase.discover_tiles)
+
+        return game.new_phase(Phase.move_player).set_turn((game.turn + 1) % len(game.players))
 
     def rotate_discovered_tile_rotate_tile(
         self, game: Game, player: PlayerColor, move: RotateTile
